15195560130dcd2a6904764862a920b0706df75c
Mufti Alie S <iyakiwan19@gmail.com>
2021-01-02 14:17:06 +0700

    Fix Submission

--- core/src/main/java/com/project/kotlin/submissionmade/core/di/CoreModule.kt
+++ core/src/main/java/com/project/kotlin/submissionmade/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.project.kotlin.submissionmade.core.data.source.remote.RemoteDataSourc
 import com.project.kotlin.submissionmade.core.data.source.remote.network.ApiService
 import com.project.kotlin.submissionmade.core.domain.repository.IMovieRepository
 import com.project.kotlin.submissionmade.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,30 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("submission".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDatabase::class.java, "Movie.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
--- favorite/src/main/java/com/project/kotlin/submissionmade/favorite/FavoriteActivity.kt
+++ favorite/src/main/java/com/project/kotlin/submissionmade/favorite/FavoriteActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import com.project.kotlin.submissionmade.core.ui.MovieAdapter
 import com.project.kotlin.submissionmade.favorite.databinding.ActivityFavoriteBinding
 import com.project.kotlin.submissionmade.ui.detail.DetailActivity
